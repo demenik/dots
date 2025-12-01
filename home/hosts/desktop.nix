c782672a834b93ca238dbc0e9197ec6cd151d061
demenik <dominik.bernroider@icloud.com>
2025-12-01 20:44:45 +0100

    Edit desktop config

--- home/hosts/desktop.nix
+++ home/hosts/desktop.nix
@@ -1,8 +1,14 @@
 {
   wayland.windowManager.hyprland.settings = {
+    env = [
+      "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+      "GBM_BACKEND,nvidia-drm"
+      "LIBVA_DRIVER_NAME,nvidia"
+    ];
+
     monitor = [
       "HDMI-A-1,preferred,auto,1"
-      "DP-3,1080x1920@60,1920x0,1,transform,1,workspace,2"
+      "DP-3,1920x1080@60,auto,1,transform,0,workspace,2"
     ];
   };
 }
