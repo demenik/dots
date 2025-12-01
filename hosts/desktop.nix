3cead137c4f9fb716156bc9880f4147312ae0d47
demenik <dominik.bernroider@icloud.com>
2025-12-01 20:44:45 +0100

    Edit desktop config

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
+    ../nixos/optional/nvidia.nix
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod"];
