dbb66b67a4fd1c317ed6f57d8aa62445dca4cb33
demenik <dominik.bernroider@icloud.com>
2025-12-01 21:14:05 +0100

    fix: Move nvidia env vars

--- nixos/optional/nvidia.nix
+++ nixos/optional/nvidia.nix
@@ -10,5 +10,8 @@
 
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    LIBVA_DRIVER_NAME = "nvidia";
   };
 }
