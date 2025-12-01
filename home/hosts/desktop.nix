495d497dd1854c20f894e71798f92442b24b0117
demenik <dominik.bernroider@icloud.com>
2025-12-01 21:14:05 +0100

    fix: Move nvidia env vars

--- home/hosts/desktop.nix
+++ home/hosts/desktop.nix
@@ -1,11 +1,5 @@
 {
   wayland.windowManager.hyprland.settings = {
-    env = [
-      "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-      "GBM_BACKEND,nvidia-drm"
-      "LIBVA_DRIVER_NAME,nvidia"
-    ];
-
     monitor = [
       "HDMI-A-1,preferred,auto,1"
       "DP-3,1920x1080@60,auto,1,transform,0,workspace,2"
