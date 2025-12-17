8908c3d68a55f263022714249857f8dc470e4650
demenik <dominik.bernroider@icloud.com>
2025-12-17 13:18:04 +0100

    fix(nvim): Remove unused incline import

--- home/editors/nvim/plugins/decoration/config.lua
+++ home/editors/nvim/plugins/decoration/config.lua
@@ -1,10 +1,6 @@
-require("incline").setup {}
-
 -- vim_current_word
 vim.cmd [[
   hi CurrentWord guifg=#cba6f7
   let g:vim_current_word#hightlight_twins = 0
   let g:vim_current_word#excluded_filetypes = ["minifiles", "netrw", "snacks_dashboard"]
 ]]
-
-local g = vim.g
