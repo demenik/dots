0e22643270db6acc87f860ae04a5afde9f31823e
demenik <dominik.bernroider@icloud.com>
2025-12-17 12:25:43 +0100

    refactor(nvim): Move snacks.dashboard to separate file

--- home/editors/nvim/plugins/utils/snacks/default.nix
+++ home/editors/nvim/plugins/utils/snacks/default.nix
@@ -11,59 +11,7 @@
       image.enabled = true;
       quickfile.enabled = true;
       rename.enabled = true;
-      dashboard = let
-        themes = import ./themes.nix;
-        theme = themes.dg_baby;
-      in {
-        enabled = true;
-        preset = {
-          inherit (theme) header;
-          keys = [
-            {
-              icon = " ";
-              key = "f";
-              desc = "Find File";
-              action = ":lua Snacks.dashboard.pick('files')";
-            }
-            {
-              icon = " ";
-              key = "n";
-              desc = "New File";
-              action = ":ene | startinsert";
-            }
-            {
-              icon = " ";
-              key = "l";
-              desc = "Live Grep";
-              action = ":lua Snacks.dashboard.pick('live_grep')";
-            }
-            {
-              icon = " ";
-              key = "o";
-              desc = "Old Files";
-              action = ":lua Snacks.dashboard.pick('oldfiles')";
-            }
-            {
-              icon = " ";
-              key = "q";
-              desc = "Quit";
-              action = ":qa";
-            }
-          ];
-        };
-        sections = [
-          {section = "header";}
-          {
-            section = "keys";
-            gap = 1;
-            padding = 1;
-          }
-          {
-            title = theme.quote;
-            align = "center";
-          }
-        ];
-      };
+      dashboard = import ./dashboard.nix;
     };
   };
 
