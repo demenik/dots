f88fc5a3b528647058424c55518e172c688156a1
demenik <dominik.bernroider@icloud.com>
2025-12-17 16:54:01 +0100

    feat(nvim): Modify nvim-tree

--- home/editors/nvim/plugins/editing/default.nix
+++ home/editors/nvim/plugins/editing/default.nix
@@ -27,9 +27,24 @@
     nvim-tree = {
       enable = true;
       settings = {
+        hijack_cursor = true;
+        view.width = {
+          min = 30;
+          max = -1;
+        };
         renderer = {
           group_empty = true;
+          icons = {
+            show.modified = true;
+            glyphs.modified = "";
+          };
+          indent_markers.enable = true;
         };
+        modified = {
+          enable = true;
+          show_on_dirs = false;
+        };
+        update_focused_file.enable = true;
       };
     };
   };
