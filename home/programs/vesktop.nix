8ed4624d41e704cb24f695a7b198f8d3cd94c9d2
demenik <dominik.bernroider@icloud.com>
2025-12-27 13:22:43 +0100

    fix(hosts): Change workspace and monitor rules for desktop

--- home/programs/vesktop.nix
+++ home/programs/vesktop.nix
@@ -3,17 +3,13 @@
 in {
   stylix.targets.vesktop.enable = false;
 
-  wayland.windowManager.hyprland.settings.windowrulev2 =
-    [
-      "workspace 4, class:^(vesktop)$"
-    ]
-    ++ map (rule: "${rule}, class:^(vesktop)$, title:^(Discord Popout)$") [
-      "float"
-      "size 640 360"
-      "keepaspectratio"
-      "pin"
-      "move 100%-w-5 100%-w-5"
-    ];
+  wayland.windowManager.hyprland.settings.windowrulev2 = map (rule: "${rule}, class:^(vesktop)$, title:^(Discord Popout)$") [
+    "float"
+    "size 640 360"
+    "keepaspectratio"
+    "pin"
+    "move 100%-w-5 100%-w-5"
+  ];
 
   programs.vesktop = {
     enable = true;
