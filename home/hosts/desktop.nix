c5d7de3a8b5d5e230e58ae6fe229489b86214e12
demenik <dominik.bernroider@icloud.com>
2025-12-27 13:08:53 +0100

    fix(hosts): Switch to team red

--- home/hosts/desktop.nix
+++ home/hosts/desktop.nix
@@ -1,8 +1,14 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "HDMI-A-1,preferred,auto,1"
-      "DP-3,1920x1080@60,auto,1,transform,0,workspace,2"
+      "HDMI-A-1, 1920x1080@143.98, 0x0, 1"
+      "DP-1, 1920x1080@60, 1920x0, 1, transform, 1"
     ];
+    workspace =
+      [
+        "2, monitor:DP-1, default:true, persistent:true"
+        "1, monitor:HDMI-A-1, default:true, persistent:true"
+      ]
+      ++ map (i: "${builtins.toString i}, monitor:HDMI-A-1") [3 4 5 6 7 8 9 10];
   };
 }
