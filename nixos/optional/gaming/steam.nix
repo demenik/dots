7bc72aa62334cd9aec4c0a56cf377452ef2ca4a1
demenik <mail@demenik.dev>
2025-12-28 17:42:23 +0100

    refactor(gaming): Move steam to separate file

---
+++ nixos/optional/gaming/steam.nix
@@ -0,0 +1,18 @@
+{user, ...}: {
+  programs.steam = {
+    enable = true;
+
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+
+    extest.enable = true;
+  };
+
+  home-manager.users.${user} = {
+    wayland.windowManager.hyprland.settings.windowrulev2 = map (rule: "${rule}, class:^(steam)$, title:^(Sign in to Steam)$") [
+      "float"
+      "center"
+    ];
+  };
+}
