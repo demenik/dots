cf29b712303a89bf7033b83117aa01197834ceb4
demenik <mail@demenik.dev>
2025-12-28 18:12:14 +0100

    feat(gaming): Add mangohud overlay

---
+++ nixos/optional/gaming/mangohud.nix
@@ -0,0 +1,30 @@
+{user, ...}: {
+  home-manager.users.${user} = {
+    programs.mangohud = {
+      enable = true;
+      settings = {
+        toggle_hud = "Shift_R+F12";
+
+        position = "top-left";
+        text_outline = true;
+        round_corners = 8.0;
+        table_columns = 4;
+
+        fps = true;
+        framtime = true;
+
+        cpu_stats = true;
+        cpu_temp = true;
+        cpu_mhz = true;
+
+        gpu_stats = true;
+        gpu_temp = true;
+        gpu_core_clock = true;
+        gpu_mem_clock = true;
+
+        ram = true;
+        vram = true;
+      };
+    };
+  };
+}
