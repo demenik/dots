25449bdc2179edf75900270e87278f6de9423957
demenik <mail@demenik.dev>
2026-01-05 19:42:53 +0100

    fix(gaming): Escape backslash

--- nixos/optional/gaming/hyprland.nix
+++ nixos/optional/gaming/hyprland.nix
@@ -2,7 +2,7 @@
   home-manager.users.${user} = {
     wayland.windowManager.hyprland.settings.windowrulev2 = let
       gameClasses = [
-        "steam_app_\d+"
+        "steam_app_\\d+"
         "lutris"
         "osu!"
         "Minecraft.*"
