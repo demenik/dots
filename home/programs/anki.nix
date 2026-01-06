1de380cc28a3712d0cc1d9562205d1b29e11372a
demenik <mail@demenik.dev>
2026-01-06 12:36:45 +0100

    feat(anki): Add discord addon

--- home/programs/anki.nix
+++ home/programs/anki.nix
@@ -20,6 +20,18 @@ in {
       passfail2
       review-heatmap
       reviewer-refocus-card
+
+      (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+        pname = "ankidiscord";
+        version = "2.0";
+        src = pkgs.fetchFromGitHub {
+          owner = "Monacraft";
+          repo = "AnkiDiscord";
+          rev = "f98fae9db30dd815dacdb58d374a12528d1b0f53";
+          hash = "sha256-iNqrYJEJomh1nIBC+IUKYEETPgzqwRBjkuBhDLmukxk=";
+        };
+        sourceRoot = finalAttrs.src.name;
+      }))
     ];
   };
 }
