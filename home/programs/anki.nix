1771121e874ed38b71700daa6ec550fcd6b7c1b4
demenik <mail@demenik.dev>
2026-01-06 13:21:33 +0100

    feat(anki): Add kanjigrid addon

--- home/programs/anki.nix
+++ home/programs/anki.nix
@@ -32,6 +32,25 @@ in {
         };
         sourceRoot = finalAttrs.src.name;
       }))
+
+      (pkgs.anki-utils.buildAnkiAddon (finalAttrs: {
+        pname = "kanjigrid";
+        version = "2.5.2";
+        src = pkgs.fetchFromGitHub {
+          owner = "Kuuuube";
+          repo = "kanjigrid";
+          rev = "f09c7dfd12bad33324f83035ae948ec0a76aa8c4";
+          sparseCheckout = ["src" "data" "manifest.json"];
+          hash = "sha256-2DZaJ/1auWEggTqDvJja3z39+uZw5Plkg0uLzHLyqnk=";
+        };
+        postPatch = ''
+          mv src/* .
+          rmdir src
+
+          substituteInPlace data.py \
+            --replace 'cwd + "/user_files/data"' 'os.path.expanduser("~/.local/share/Anki2/kanjigrid_user_files/data")'
+        '';
+      }))
     ];
   };
 }
