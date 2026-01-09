ecbe72563ae10444a85da987a85e950542099a3f
demenik <mail@demenik.dev>
2026-01-09 19:21:57 +0100

    fix(scripts): Use writeShellApplication

--- home/shells/scripts/default.nix
+++ home/shells/scripts/default.nix
@@ -4,43 +4,58 @@
   ...
 }: {
   home.packages = with pkgs; [
-    (
-      writeScriptBin "rebuild"
-      # bash
-      ''
-        cd "${dotsDir}" || exit
-        git add --intent-to-add . 2>/dev/null
-        nixos-rebuild $@ switch --flake . --sudo
-        cd - >/dev/null || exit
-      ''
-    )
-    (
-      writeScriptBin "update"
-      # bash
-      ''
-        cd "${dotsDir}" || exit
-        git add --intent-to-add . 2>/dev/null
-        nix flake $@ update
-        cd - >/dev/null || exit
-      ''
-    )
-    (
-      writeScriptBin "nix"
-      # bash
-      ''
-        NIX=${pkgs.lib.getExe pkgs.nix}
+    (writeShellApplication {
+      name = "rebuild";
+      text =
+        # bash
+        ''
+          cd "${dotsDir}" || exit
+          git add --intent-to-add . 2>/dev/null
+          nixos-rebuild "$@" switch --flake . --sudo
+          cd - >/dev/null || exit
+        '';
+    })
+    (writeShellApplication {
+      name = "update";
+      text =
+        # bash
+        ''
+          cd "${dotsDir}" || exit
+          git add --intent-to-add . 2>/dev/null
+          nix flake "$@" update
+          cd - >/dev/null || exit
+        '';
+    })
+    (writeShellApplication {
+      name = "nix";
+      text =
+        # bash
+        ''
+          NIX=${pkgs.lib.getExe pkgs.nix}
 
-        if [[ $1 == "develop" || $1 == "shell" ]]; then
-          cmd=$1
-          shift
-          exec "$NIX" "$cmd" -c "$SHELL" "$@"
-        else
-          exec "$NIX" "$@"
-        fi
-      ''
-    )
-    (writeScriptBin "git-changes" (builtins.readFile ./git-changes.sh))
-    (writeScriptBin "copy" (builtins.readFile ./copy.sh))
-    (writeScriptBin "boot-next" (import ./boot-next.sh.nix pkgs))
+          if [[ $1 == "develop" || $1 == "shell" ]]; then
+            cmd=$1
+            shift
+            exec "$NIX" "$cmd" -c "$SHELL" "$@"
+          else
+            exec "$NIX" "$@"
+          fi
+        '';
+    })
+    (writeShellApplication {
+      name = "git-changes";
+      text = builtins.readFile ./git-changes.sh;
+    })
+    (writeShellApplication {
+      name = "copy";
+      text = builtins.readFile ./copy.sh;
+    })
+    (writeShellApplication {
+      name = "boot-next";
+      runtimeInputs = [
+        efibootmgr
+      ];
+      text = builtins.readFile ./boot-next.sh;
+    })
   ];
 }
