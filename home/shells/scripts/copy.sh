f1fb660704e2b46f343887e1f75007898ec800a2
demenik <mail@demenik.dev>
2026-01-09 20:10:24 +0100

    fix(scripts): Fix variable not defined

--- home/shells/scripts/copy.sh
+++ home/shells/scripts/copy.sh
@@ -1,28 +1,28 @@
 tmpfile=$(mktemp)
 trap 'rm -f "$tmpfile"' EXIT
 
-cat > "$tmpfile"
+cat >"$tmpfile"
 
 if [ -t 1 ]; then
   {
-    if [ -n "$TMUX" ] || [ "${TERM%%-*}" = "screen" ]; then
+    if [ -n "${TMUX:-}" ] || [ "${TERM%%-*}" = "screen" ]; then
       printf "\033Ptmux;\033\033]52;c;"
-      base64 < "$tmpfile" | tr -d '\n'
+      base64 <"$tmpfile" | tr -d '\n'
       printf "\007\033\\"
     else
       printf "\033]52;c;"
-      base64 < "$tmpfile" | tr -d '\n'
+      base64 <"$tmpfile" | tr -d '\n'
       printf "\007"
     fi
-  } > /dev/tty
+  } >/dev/tty
 fi
 
 if command -v wl-copy >/dev/null 2>&1 && [ -n "$WAYLAND_DISPLAY" ]; then
   cat "$tmpfile" | wl-copy
 fi
 
-lines=$(wc -l < "$tmpfile")
-size=$(wc -c < "$tmpfile")
+lines=$(wc -l <"$tmpfile")
+size=$(wc -c <"$tmpfile")
 readable_size=$(numfmt --to=iec-i --suffix=B "$size")
 
 echo "Copied $lines lines ($readable_size)" >&2
