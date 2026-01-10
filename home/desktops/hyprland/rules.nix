64005846be92a9481d76b8e439545c141f0713d3
demenik <mail@demenik.dev>
2026-01-10 21:27:31 +0100

    feat(hyprland): Add noinitialfocus for every non-floating window

--- home/desktops/hyprland/rules.nix
+++ home/desktops/hyprland/rules.nix
@@ -6,6 +6,8 @@
       "float, class:^(hyprland-share-picker)$"
       "workspace 3, class:^(Electron)$, title:^(BSC)$" # BetterSoundCloud
       "workspace 3, class:^(opensoundcloud)$"
+
+      "noinitialfocus, floating:0" # Dont focus tiling windows on startup
     ]
     ++ map (rule: "${rule}, class:^(xdg-desktop-portal-gtk)$") [
       "float"
