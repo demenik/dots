c82a81062256647986d243ca1c84e335fc42731a
demenik <mail@demenik.dev>
2026-01-11 16:19:16 +0100

    feat(hyprlock): Update style

--- home/desktops/hyprland/hyprlock.nix
+++ home/desktops/hyprland/hyprlock.nix
@@ -7,101 +7,127 @@
     enable = true;
 
     settings = let
-      profilePhoto = "$HOME/Pictures/pfp/bladee/egobaby.jpg";
-      wallpaper = "$HOME/Pictures/wallpapers/takopi/class.png";
+      profilePhoto = "/var/lib/greeter-assets/user.png";
       font_family = config.stylix.fonts.sansSerif.name;
 
       inherit (config.lib.stylix) colors;
+      background = colors.base00;
       text = colors.base05;
       yellow = colors.base0A;
+      red = colors.base08;
       accent = colors.base0D;
     in {
       authentication.fingerprint.enabled = true;
 
-      background = {
-        monitor = "";
-        path = wallpaper;
-        blur_passes = 0;
-        blur_size = 8;
-      };
+      background = lib.mkForce [
+        {
+          monitor = "";
+          path = "screenshot";
+          color = "rgb(${background})";
+
+          blur_size = 4;
+          blur_passes = 3;
+          noise = 0.0117;
+          contrast = 0.8916;
+          brightness = 0.8172;
+          vibrancy = 0.1696;
+          vibrancy_darkness = 0.0;
+        }
+      ];
 
       general = {
         hide_cursor = true;
-        ignore_empty_input = true;
+        ignore_empty_input = false;
+        grace = 0;
+        no_fade_in = false;
       };
 
-      input-field = {
-        monitor = "";
+      input-field = lib.mkForce (map (monitor: {
+        inherit monitor;
         size = "250, 60";
         position = "0, -225";
+
         outline_thickness = 2;
         dots_size = 0.2;
         dots_spacing = 0.2;
         dots_center = true;
-        outer_color = lib.mkForce "rgba(0, 0, 0, 0)";
-        inner_color = lib.mkForce "rgba(100, 114, 125, 0.4)";
-        font_color = lib.mkForce "rgb(${text})";
         fade_on_empty = false;
-        inherit font_family;
-        placeholder_text = "<span foreground=\"##${text}99\"><i>󰌾 Login as </i><span foreground=\"##${accent}ff\">$USER</span></span>";
-        fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
-        capslock_color = "rgb(${yellow})";
-        check_color = lib.mkForce "rgb(${accent})";
-      };
 
-      label = [
-        # Layout
+        inherit font_family;
+        outer_color = "rgba(0, 0, 0, 0)";
+        inner_color = "rgba(100, 114, 125, 0.4)";
+        font_color = "rgb(${text})";
+        capslock_color = "rgb(${yellow})";
+        check_color = "rgb(${accent})";
+        fail_color = "rgb(${red})";
+
+        placeholder_text =
+          # html
+          ''
+            <i><span foreground="##${text}99"> 󰌾 Enter Password... </span></i>
+          '';
+        fail_text =
+          # html
+          ''
+            <i>$FAIL <b>($ATTEMPTS)</b></i>
+          '';
+      }) ["HDMI-A-1" "eDP-1"]);
+
+      label = builtins.concatMap (monitor: [
+        # Keyboard Layout
         {
-          monitor = "";
-          text = "Layout: $LAYOUT";
-          color = "#rgb(${text})";
+          inherit monitor;
+          text = "$LAYOUT";
           font_size = 12;
-          inherit font_family;
           position = "30, -30";
           halign = "left";
           valign = "top";
+
+          inherit font_family;
+          color = "#rgba(${text}77)";
         }
         # Time
         {
-          monitor = "";
+          inherit monitor;
           text = "cmd[update:1000] echo \"$(date +\"%H:%M\")\"";
-          color = "rgb(${text})";
           font_size = 130;
-          inherit font_family;
           position = "0, 240";
+
+          color = "rgb(${text})";
+          inherit font_family;
         }
         # Day-Month
         {
-          monitor = "";
+          inherit monitor;
           text = "cmd[update:43200000] echo -e \"$(date +\"%A, %d. %B %Y\")\"";
-          color = "rgb(${text})";
           font_size = 30;
-          inherit font_family;
           position = "0, 105";
-        }
-        # Fingerprint
-        {
-          monitor = "";
-          text = "$FPRINTPROMPT";
-          color = "rgb(${text})";
-          font_size = 14;
-          inherit font_family;
-          position = "0, -130";
-        }
-      ];
 
-      # image = [
-      #   # Profile Photo
-      #   {
-      #     monitor = "";
-      #     path = profilePhoto;
-      #     border_size = 0;
-      #     size = 120;
-      #     rounding = -1;
-      #     rotate = 0;
-      #     position = "0, -20";
-      #   }
-      # ];
+          color = "rgb(${text})";
+          inherit font_family;
+        }
+        # Username
+        {
+          inherit monitor;
+          text = "$USER";
+          font_size = 24;
+          position = "0, -130";
+
+          color = "rgb(${text})";
+          inherit font_family;
+        }
+      ]) ["HDMI-A-1" "eDP-1"];
+
+      image = map (monitor: {
+        # Profile Photo
+        inherit monitor;
+        path = profilePhoto;
+        size = 120;
+        position = "0, -20";
+
+        border_size = 0;
+        rounding = 0;
+      }) ["HDMI-A-1" "eDP-1"];
     };
   };
 }
