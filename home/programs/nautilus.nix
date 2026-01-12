a3b3fb0064ab75717e5fcbbd7c2d679471d9ba45
demenik <mail@demenik.dev>
2026-01-12 12:30:14 +0100

    fix(nautilus): Fix open terminal here

--- home/programs/nautilus.nix
+++ home/programs/nautilus.nix
@@ -2,17 +2,27 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  nautilus-wrapped = pkgs.symlinkJoin {
+    name = "nautilus";
+    paths = [pkgs.nautilus];
+    buildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/nautilus \
+        --prefix PYTHONPATH : "${pkgs.python3.pkgs.pygobject3}/${pkgs.python3.sitePackages}" \
+        --set NAUTILUS_4_EXTENSION_DIR "${pkgs.nautilus-python}/lib/nautilus/extensions-4"
+    '';
+  };
+in {
   home.packages = with pkgs; [
-    nautilus
+    nautilus-wrapped
     nautilus-python
     nautilus-open-any-terminal
   ];
 
-  dconf.settings."com/github/Stunkymonkey/nautilus-open-any-terminal" = {
-    terminal = "kitty";
-    new-tab = true;
-    flatpak = "system";
+  dconf.settings."com/github/stunkymonkey/nautilus-open-any-terminal" = {
+    terminal = config.home.sessionVariables.TERMINAL;
+    lockAll = true;
   };
 
   gtk = {
