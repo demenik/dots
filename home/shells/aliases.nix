c96e6b08e3b3873da5d679ea3bd754b83bce3d29
demenik <mail@demenik.dev>
2026-01-13 14:44:57 +0100

    feat(shells): Add zipx alias (exclude preset)

--- home/shells/aliases.nix
+++ home/shells/aliases.nix
@@ -3,9 +3,12 @@
     v = "nvim";
     c = "clear";
     cp = "cp -riv";
+    zip = "zip -r";
     ssh = "kitten ssh";
     hm = "home-manager";
 
     "nix-shell" = "nix-shell --command zsh";
+
+    zipx = "zip -x '*.direnv*' -x '*node_modules*' -x '*build*' -x '*target*' -x '*.git*' -x '*.DS_Store*' -r";
   };
 }
