e357d8fadc493d1457edf585d82c12281441dfc4
demenik <mail@demenik.dev>
2026-01-17 16:29:49 +0100

    fix(git): Move to separate file

--- home/global.nix
+++ home/global.nix
@@ -3,78 +3,18 @@
   pkgs,
   stateVersion,
   lib,
-  config,
   ...
 }: {
+  imports = [
+    ./git.nix
+  ];
+
   nix = {
     package = lib.mkDefault pkgs.nix;
     settings.experimental-features = ["nix-command" "flakes"];
   };
 
-  programs = {
-    home-manager.enable = true;
-    git = {
-      enable = true;
-      settings = {
-        commit.gpgsign = true;
-        tag.gpgsign = true;
-        gpg.format = "ssh";
-        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-
-        url = let
-          httpsToSsh = {
-            host,
-            user ? "git",
-          }: {
-            "ssh://${user}@${host}".insteadOf = "https://${host}";
-          };
-        in
-          httpsToSsh {host = "github.com";}
-          // httpsToSsh {host = "gitlab.uni-ulm.de";}
-          // httpsToSsh {host = "gitea.demenik.dev";};
-
-        user = {
-          email = "mail@demenik.dev";
-          name = "demenik";
-          signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
-        };
-
-        init.defaultBranch = "main";
-      };
-
-      includes = let
-        mkRemoteSsh = {
-          user ? "git",
-          url,
-          config,
-        }: {
-          condition = "hasconfig:remote.*.url:${user}@${url}:*/**";
-          path = "${pkgs.writeText "gitconfig-${url}" (pkgs.lib.generators.toGitINI config)}";
-        };
-      in [
-        (mkRemoteSsh {
-          url = "gitlab.uni-ulm.de";
-          config.user = {
-            email = "dominik.bernroider@uni-ulm.de";
-            name = "Dominik Bernroider";
-          };
-        })
-        (mkRemoteSsh {
-          url = "gitea.demenik.dev";
-          config.user = {
-            email = "mail@demenik.dev";
-            name = "demenik";
-          };
-        })
-      ];
-    };
-    gpg.enable = true;
-  };
-
-  services.gpg-agent = {
-    enable = true;
-    pinentry.package = pkgs.pinentry-curses;
-  };
+  programs.home-manager.enable = true;
 
   home = {
     inherit stateVersion;
