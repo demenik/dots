8add16737fc42232e19dc2bd67a3e513b818e43a
demenik <mail@demenik.dev>
2026-01-18 15:07:05 +0100

    fix(nvim): Fix desktop entry

--- home/editors/nvim/default.nix
+++ home/editors/nvim/default.nix
@@ -1,4 +1,8 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   imports = [
     inputs.nixvim.homeModules.nixvim
     ./plugins
@@ -56,5 +60,31 @@
           }
         end
       '';
+  };
+
+  xdg.desktopEntries.nvim = {
+    name = "Neovim";
+    genericName = "Text Editor";
+    exec = "${config.home.sessionVariables.TERMINAL} -e nvim %F";
+    terminal = false;
+    categories = ["Utility" "TextEditor"];
+    mimeType = [
+      "text/english"
+      "text/plain"
+      "text/x-makefile"
+      "text/x-c++hdr"
+      "text/x-c++src"
+      "text/x-chdr"
+      "text/x-csrc"
+      "text/x-java"
+      "text/x-moc"
+      "text/x-pascal"
+      "text/x-tcl"
+      "text/x-tex"
+      "application/x-shellscript"
+      "text/x-c"
+      "text/x-c++"
+    ];
+    icon = "nvim";
   };
 }
