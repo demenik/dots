bab82fc4587fc6ca5c158b36b22da988f005a237
demenik <mail@demenik.dev>
2026-01-18 15:23:05 +0100

    fix(kitty): Use writeShellApplication for kitty-cwd

--- home/terminals/kitty/cwd.nix
+++ home/terminals/kitty/cwd.nix
@@ -1,40 +1,43 @@
 {pkgs, ...}: let
-  hyprctl = pkgs.lib.getExe' pkgs.hyprland "hyprctl";
-  jq = pkgs.lib.getExe pkgs.jq;
-  pgrep = pkgs.lib.getExe' pkgs.procps "pgrep";
-  kitty = pkgs.lib.getExe pkgs.kitty;
-
   # A kitty wrapper, to launch a new kitty window at the same path of the currently focused kitty window.
   # Only works with zsh
-  kitty-cwd =
-    pkgs.writeShellScriptBin "kitty-cwd"
-    # sh
-    ''
-      active_window="$(${hyprctl} activewindow -j)"
-      class="$(echo "$active_window" | ${jq} -r .class)"
-      pid="$(echo "$active_window" | ${jq} -r .pid)"
+  kitty-cwd = pkgs.writeShellApplication {
+    name = "kitty-cwd";
+    runtimeInputs = with pkgs; [
+      hyprland
+      jq
+      procps
+      kitty
+    ];
+    text =
+      # bash
+      ''
+        active_window="$(hyprctl activewindow -j)"
+        class="$(echo "$active_window" | jq -r .class)"
+        pid="$(echo "$active_window" | jq -r .pid)"
 
-      leaf_cwd=""
+        leaf_cwd=""
 
-      if [[ "$class" == "kitty" ]]; then
-        current_pid="$pid"
+        if [[ "$class" == "kitty" ]]; then
+          current_pid="$pid"
 
-        while true; do
-          child_pid=$(${pgrep} -P "$current_pid" -n)
-          if [[ -z "$child_pid" ]]; then break; fi
+          while true; do
+            child_pid=$(pgrep -P "$current_pid" -n)
+            if [[ -z "$child_pid" ]]; then break; fi
 
-          cwd_file="''${XDG_RUNTIME_DIR:-/tmp}/kitty-cwd-$child_pid"
-          if [[ -f "$cwd_file" ]]; then
-            leaf_cwd=$(cat "$cwd_file")
-          fi
+            cwd_file="''${XDG_RUNTIME_DIR:-/tmp}/kitty-cwd-$child_pid"
+            if [[ -f "$cwd_file" ]]; then
+              leaf_cwd=$(cat "$cwd_file")
+            fi
 
-          current_pid="$child_pid"
-        done
-      fi
+            current_pid="$child_pid"
+          done
+        fi
 
-      target="''${leaf_cwd:-$HOME}"
-      ${kitty} --directory "$target"
-    '';
+        target="''${leaf_cwd:-$HOME}"
+        kitty --directory "$target"
+      '';
+  };
 in {
   home.packages = [
     kitty-cwd
@@ -47,10 +50,10 @@ in {
     };
 
     zsh.initContent =
-      # sh
+      # zsh
       ''
         write_cwd() {
-          echo "$PWD" > "''${XDG_RUNTIME_DIR:-/tmp}/kitty-cwd-$$"
+          echo "$PWD" >"''${XDG_RUNTIME_DIR:-/tmp}/kitty-cwd-$$"
         }
         precmd_functions+=(write_cwd)
 
