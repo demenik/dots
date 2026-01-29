5331a5c821478fd39d117cc873bb0198ad8c36cc
demenik <mail@demenik.dev>
2026-01-29 23:18:58 +0100

    fix: Remove nix.gc as nh handles garbage collection now

--- nixos/global/nix.nix
+++ nixos/global/nix.nix
@@ -1,18 +1,12 @@
 {pkgs, ...}: {
   nix = {
     package = pkgs.nix;
-
     settings = {
       auto-optimise-store = true;
       trusted-users = ["root" "@wheel"];
       experimental-features = "nix-command flakes";
       warn-dirty = false;
       download-buffer-size = 512 * 1024 * 1024; # 512 MiB
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
     };
   };
 
