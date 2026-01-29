7bb04b94b179f04f644ef001a47531c697a2cbf3
demenik <mail@demenik.dev>
2026-01-29 23:03:37 +0100

    fix: Remove unused nixd.nix

--- nixd.nix
+++
@@ -1,9 +0,0 @@
-let
-  flake = builtins.getFlake (toString ./.);
-  hmConfig = flake.homeConfigurations."demenik@desktop";
-in
-{
-  nixos = flake.nixosConfigurations.desktop.options;
-  home_manager = hmConfig.options;
-  nixvim = hmConfig.options.programs.nixvim.type.getSubOptions [];
-}
