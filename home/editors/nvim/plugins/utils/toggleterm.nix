698b1309c7beefe21c32d1b7b54d4722101ca7ee
demenik <mail@demenik.dev>
2026-02-01 18:44:49 +0100

    fix(nvim): Remove lazygit settings from toggleterm

--- home/editors/nvim/plugins/utils/toggleterm.nix
+++ home/editors/nvim/plugins/utils/toggleterm.nix
@@ -2,13 +2,9 @@
   plugins.toggleterm = {
     enable = true;
     settings = {
-      float_opts = {};
       persistent_size = true;
       direction = "horizontal";
       open_mapping = "[[<M-CR>]]";
-      shade_filetypes = ["lazygit"];
-
-      highlights.FloatBorder.guifg = "#7b8496";
     };
   };
 }
