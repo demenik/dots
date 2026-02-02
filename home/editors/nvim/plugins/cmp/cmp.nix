a26ec529cf6e95e0818984290edaf3fdb7c551c3
demenik <mail@demenik.dev>
2026-02-02 23:09:32 +0100

    fix(nvim): Fix cmp icons

--- home/editors/nvim/plugins/cmp/cmp.nix
+++ home/editors/nvim/plugins/cmp/cmp.nix
@@ -68,11 +68,13 @@
               # lua
               ''
                 function(entry, vim_item)
-                  local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
-                  local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                  kind.kind = " " .. (strings[1] or "") .. " "
-                  kind.menu = "    (" .. (strings[2] or "") .. ")"
-                  return kind
+                  local lspkind = require "lspkind"
+                  local kind_name = vim_item.kind
+                  local icon = lspkind.symbol_map[kind_name] or kind_name
+
+                  vim_item.kind = " " .. icon .. " "
+                  vim_item.menu = "    (" .. kind_name .. ")"
+                  return vim_item
                 end
               '';
           };
