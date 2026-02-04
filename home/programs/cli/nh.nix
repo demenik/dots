1c5a7476ed432d0c7bd71bb37d14b0511e8a2151
demenik <mail@demenik.dev>
2026-02-04 16:23:34 +0100

    fix(nh): Add git intent-to-add to nh

--- home/programs/cli/nh.nix
+++ home/programs/cli/nh.nix
@@ -1,7 +1,26 @@
-{dotsDir, ...}: {
+{
+  pkgs,
+  lib,
+  dotsDir,
+  ...
+}: let
+  nh-wrapped = pkgs.writeShellApplication {
+    name = "nh";
+    runtimeInputs = with pkgs; [git nh];
+    text = ''
+      export NH_FLAKE="${dotsDir}"
+
+      if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+        git add --intent-to-add .
+      fi
+
+      exec ${lib.getExe pkgs.nh} "$@"
+    '';
+  };
+in {
   programs.nh = {
     enable = true;
-    flake = dotsDir;
+    package = nh-wrapped;
 
     clean = {
       enable = true;
