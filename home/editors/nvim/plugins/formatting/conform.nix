b82fd3c3b4fa21a2bd7fd1b4640c7183f36289c9
demenik <mail@demenik.dev>
2026-02-12 23:56:43 +0100

    fix(nvim): Make conform run only after save

--- home/editors/nvim/plugins/formatting/conform.nix
+++ home/editors/nvim/plugins/formatting/conform.nix
@@ -78,43 +78,9 @@
           # "_" = ["trim_whitespace" "trim_newlines"];
         };
 
-        format_on_save =
-          # lua
-          ''
-            function(bufnr)
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              if vim.g.slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
-
-              local function on_format(err)
-                if err and err:match("timeout$") then
-                  slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                end
-              end
-
-              return { timeout_ms = 200, lsp_fallback = true }, on_format
-            end
-          '';
-
-        format_after_save =
-          # lua
-          ''
-            function(bufnr)
-              if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                return
-              end
-
-              if not vim.g.slow_format_filetypes[vim.bo[bufnr].filetype] then
-                return
-              end
-
-              return { lsp_fallback = true }
-            end
-          '';
+        format_after_save = {
+          lsp_fallback = true;
+        };
       };
     };
 
