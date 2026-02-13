5694eded891a5ba69eed3a77807d902e883e9f7b
demenik <mail@demenik.dev>
2026-02-13 18:34:13 +0100

    fix(wine): Add mime default applications

--- nixos/optional/wine.nix
+++ nixos/optional/wine.nix
@@ -1,11 +1,22 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   environment.systemPackages = with pkgs; [
     wineWowPackages.full
+    winetricks
   ];
+
+  xdg.mime.defaultApplications = {
+    "application/x-ms-dos-executable" = "wine.desktop";
+    "application/x-msdownload" = "wine.desktop";
+    "application/exe" = "wine.desktop";
+  };
 
   boot.binfmt.registrations.wine = {
     recognitionType = "magic";
     magicOrExtension = "MZ";
-    interpreter = pkgs.wineWowPackages.full;
+    interpreter = lib.getExe pkgs.wineWowPackages.full;
   };
 }
