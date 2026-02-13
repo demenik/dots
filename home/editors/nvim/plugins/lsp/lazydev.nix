1ba2ab871a0929b6a6f435b6599b66ffbbcb80ad
demenik <mail@demenik.dev>
2026-02-13 22:53:55 +0100

    fix(nvim): Fix https://github.com/folke/lazydev.nvim/issues/136

--- home/editors/nvim/plugins/lsp/lazydev.nix
+++ home/editors/nvim/plugins/lsp/lazydev.nix
@@ -1,13 +1,21 @@
 {pkgs, ...}: {
-  programs.nixvim.plugins.lazydev = {
-    enable = true;
-    settings = {
-      library = [
-        {
-          path = "${pkgs.vimPlugins.snacks-nvim}";
-          words = ["Snacks"];
-        }
-      ];
+  programs.nixvim = {
+    plugins.lazydev = {
+      enable = true;
+      settings = {
+        library = [
+          {
+            path = "${pkgs.vimPlugins.snacks-nvim}";
+            words = ["Snacks"];
+          }
+        ];
+      };
+    };
+
+    lsp.servers = {
+      lua_ls.config = {
+        workspace.library.__raw = ''vim.api.nvim_get_runtime_file("", true)'';
+      };
     };
   };
 }
