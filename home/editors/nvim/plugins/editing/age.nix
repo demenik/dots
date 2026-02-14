642cc83f8a5a7b3ad965a6906c1843035dd8d0eb
demenik <mail@demenik.dev>
2026-02-14 17:59:15 +0100

    feat(nvim): Update age editing Wrap rage to allow creating age files from neovim Removed obsolete secrets.nix, as agenix -e doesn't work with the neovim plugin

--- home/editors/nvim/plugins/editing/age.nix
+++ home/editors/nvim/plugins/editing/age.nix
@@ -1,4 +1,13 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (config.home) homeDirectory;
+  identityFile = "${homeDirectory}/.ssh/id_agenix";
+  pubKeyFile = "${homeDirectory}/.ssh/id_agenix.pub";
+
   age-secret-nvim = pkgs.vimUtils.buildVimPlugin {
     name = "age-secret";
     src = pkgs.fetchFromGitHub {
@@ -8,14 +17,44 @@
       hash = "sha256-3RMSaUfZyMq9aNwBrdVIP4Mh80HwIcO7I+YhFOw+NU8=";
     };
   };
+
+  rage-wrapped = pkgs.writeShellScriptBin "rage" ''
+    TMP_FILE=$(mktemp)
+    cat > "$TMP_FILE"
+
+    is_empty=0
+    if [ ! -s "$TMP_FILE" ]; then is_empty=1; fi
+
+    is_decrypt=0
+    for arg in "$@"; do
+      if [ "$arg" = "-d" ] || [ "$arg" = "--decrypt" ]; then
+        is_decrypt=1
+        break
+      fi
+    done
+
+    if [ "$is_empty" -eq 1 ] && [ "$is_decrypt" -eq 1 ]; then
+      rm "$TMP_FILE"
+      exit 0
+    fi
+
+    if [ "$is_decrypt" -eq 0 ]; then
+      set -- "$@" -R "${pubKeyFile}"
+    fi
+
+    cat "$TMP_FILE" | ${lib.getExe pkgs.rage} "$@"
+    EXIT_CODE=$?
+    rm "$TMP_FILE"
+    exit $EXIT_CODE
+  '';
 in {
   programs.nixvim = {
     extraPlugins = [age-secret-nvim];
-    extraPackages = [pkgs.rage];
+    extraPackages = [rage-wrapped];
 
     extraConfigLua = ''
       require("age_secret").setup {
-        identity = "~/.ssh/id_agenix",
+        identity = "${identityFile}",
       }
     '';
 
@@ -29,7 +68,7 @@ in {
       "*.age diff=age"
     ];
     settings = {
-      diff.age.textconv = "agenix -d";
+      diff.age.textconv = "${lib.getExe pkgs.rage} -i ${identityFile} -d";
     };
   };
 }
