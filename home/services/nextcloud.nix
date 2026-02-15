499ad3139652159cc4341b6b87e1ebee852c58de
demenik <mail@demenik.dev>
2026-02-15 21:19:17 +0100

    fix(nextcloud-mount): Fix icon and always restart

--- home/services/nextcloud.nix
+++ home/services/nextcloud.nix
@@ -7,11 +7,20 @@
   configFile = config.age.secrets.nextcloud.path;
   mountPoint = "${config.home.homeDirectory}/Nextcloud";
 
-  ncIcon = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/nextcloud/promo/master/nextcloud-icon.svg";
-    hash = "sha256-wqzR2retViX/kGuAkG+7doZrvfj0TAAvFVSOygqr8Fs=";
-  };
+  iconName = "nextcloud-mount";
+  iconPath = "${config.home.homeDirectory}/.local/share/icons/hicolor/scalable/apps/${iconName}.svg";
 in {
+  xdg.dataFile = {
+    "icons/hicolor/scalable/apps/${iconName}.svg".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nextcloud/desktop/refs/heads/master/theme/colored/Nextcloud-icon.svg";
+      hash = "sha256-PAYCF1PxQOJu2tl2PpggunDNrSVHZgARF56O0b3oVG4=";
+    };
+    "icons/hicolor/symbolic/apps/${iconName}-symbolic.svg".source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/nextcloud/desktop/refs/heads/master/theme/white/wizard-nextcloud.svg";
+      hash = "sha256-n1l3GVpQNnmZgrcwhWchvw8MsAmolzjxkYpJIw90PFc=";
+    };
+  };
+
   systemd.user = {
     services.nextcloud-mount = {
       Unit = {
@@ -29,14 +38,15 @@ in {
             --vfs-cache-mode full \
             --vfs-cache-max-age 24h \
             --dir-cache-time 5m \
-            --allow-other=false
+            --allow-other=false \
+            --volname "Nextcloud"
         '';
         ExecStartPost = ''
-          ${lib.getExe' pkgs.glib "gio"} set "${mountPoint}" metadata::custom-icon-name "${ncIcon}"
+          ${lib.getExe' pkgs.glib "gio"} set "${mountPoint}" metadata::custom-icon-name ${iconName}
         '';
         ExecStop = "/run/wrappers/bin/fusermount -u -z ${mountPoint}";
-        Restart = "on-failure";
-        RestartSec = "10s";
+        Restart = "always";
+        RestartSec = "5s";
       };
       Install.WantedBy = ["default.target"];
     };
