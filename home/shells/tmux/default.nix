f47d88a259afbe16dd9ab491ebd05d66bd959d96
demenik <mail@demenik.dev>
2026-02-26 16:15:22 +0100

    refactor(tmux): Move init script to new file

--- home/shells/tmux/default.nix
+++ home/shells/tmux/default.nix
@@ -1,28 +1,8 @@
 {
   imports = [
     ./plugins
+    ./init.nix
   ];
-
-  programs.zsh.initExtraFirst =
-    # zsh
-    ''
-      if [ -n "$TMUX" ]; then
-        function refresh_kitty_env() {
-          eval $(tmux show-environment -s KITTY_WINDOW_ID 2>/dev/null)
-          eval $(tmux show-environment -s KITTY_PID 2>/dev/null)
-        }
-        autoload -Uz add-zsh-hook
-        add-zsh-hook preexec refresh_kitty_env
-      fi
-
-      if [ -n "$PS1" ] && [ -z "$TMUX" ] && [ -z "$SSH_CONNECTION" ]; then
-        if tmux has-session -t main 2>/dev/null; then
-          exec tmux new-session -t main -s "main-$$" \; new-window
-        else
-          exec tmux new-session -s main
-        fi
-      fi
-    '';
 
   programs.tmux = {
     enable = true;
