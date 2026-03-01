b785dfc8ceefa98088d2e9a687f023fa54e98c6e
demenik <mail@demenik.dev>
2026-03-01 13:46:57 +0100

    feat(tmux): Replace catppuccin theme with catppuccin patched tokyo night theme

--- home/shells/tmux/plugins/default.nix
+++ home/shells/tmux/plugins/default.nix
@@ -3,19 +3,10 @@
     ./which-key.nix
     ./vim-tmux-nav.nix
     ./notify.nix
+    ./theme.nix
   ];
 
   programs.tmux.plugins = with pkgs.tmuxPlugins; [
-    {
-      plugin = catppuccin;
-      extraConfig =
-        # tmux
-        ''
-          set -g @catppuccin_flavor 'mocha'
-          set -g @catppuccin_window_tabs_enabled on
-        '';
-    }
-
     sensible # good defaults
     pain-control # binds for panes
     continuum # persistent sessions
