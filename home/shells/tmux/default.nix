da7c96ba4b59bc3fd814f6a126ca7df930ac1e92
demenik <mail@demenik.dev>
2026-03-01 14:05:59 +0100

    fix(tmux): Automatically start block selection

--- home/shells/tmux/default.nix
+++ home/shells/tmux/default.nix
@@ -31,7 +31,7 @@
         unbind [
         bind v copy-mode
         bind-key -T copy-mode-vi v send-keys -X begin-selection
-        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi C-v send-keys -X begin-selection \; send-keys -X rectangle-toggle
         bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
         unbind ]
