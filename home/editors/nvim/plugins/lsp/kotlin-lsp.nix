d13df39d6f7d3af6495f5def6dee22eabda70e0f
demenik <mail@demenik.dev>
2026-03-09 22:25:11 +0100

    fix(nvim): Fix kotlin-lsp build script for new version

--- home/editors/nvim/plugins/lsp/kotlin-lsp.nix
+++ home/editors/nvim/plugins/lsp/kotlin-lsp.nix
@@ -25,61 +25,14 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mkdir -p "$out"/bin "$out"/libexec/kotlin-lsp
-    cp -r "$src"/* "$out"/libexec/kotlin-lsp/
+    cp -r "$src"/lib "$src"/native "$src"/kotlin-lsp.sh "$out"/libexec/kotlin-lsp/
 
-    makeWrapper "${jdk21_headless}"/bin/java "$out"/bin/kotlin-lsp \
-      --add-flags "--add-opens java.base/java.io=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.lang=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.lang.ref=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.lang.reflect=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.net=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.nio=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.nio.charset=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.text=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.time=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.util=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.util.concurrent=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.util.concurrent.atomic=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/java.util.concurrent.locks=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/jdk.internal.vm=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/sun.net.dns=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/sun.nio.ch=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/sun.nio.fs=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/sun.security.ssl=ALL-UNNAMED" \
-      --add-flags "--add-opens java.base/sun.security.util=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/com.apple.eawt=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/com.apple.eawt.event=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/com.apple.laf=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/com.sun.java.swing=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/com.sun.java.swing.plaf.gtk=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/java.awt=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/java.awt.dnd.peer=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/java.awt.event=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/java.awt.font=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/java.awt.image=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/java.awt.peer=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/javax.swing=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/javax.swing.plaf.basic=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/javax.swing.text=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/javax.swing.text.html=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.awt=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.awt.X11=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.awt.datatransfer=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.awt.image=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.awt.windows=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.font=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.java2d=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.lwawt=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.lwawt.macosx=ALL-UNNAMED" \
-      --add-flags "--add-opens java.desktop/sun.swing=ALL-UNNAMED" \
-      --add-flags "--add-opens java.management/sun.management=ALL-UNNAMED" \
-      --add-flags "--add-opens jdk.attach/sun.tools.attach=ALL-UNNAMED" \
-      --add-flags "--add-opens jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED" \
-      --add-flags "--add-opens jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED" \
-      --add-flags "--add-opens jdk.jdi/com.sun.tools.jdi=ALL-UNNAMED" \
-      --add-flags "--enable-native-access=ALL-UNNAMED" \
-      --add-flags "-Djdk.lang.Process.launchMechanism=FORK" \
-      --add-flags "-cp \"$out/libexec/kotlin-lsp/lib/*\" com.jetbrains.ls.kotlinLsp.KotlinLspServerKt"
+    sed -i '/if \[ -d "$LOCAL_JRE_PATH" \]/,/fi/c\JAVA_BIN="java"' "$out"/libexec/kotlin-lsp/kotlin-lsp.sh
+    sed -i '/chmod +x/d' "$out/libexec/kotlin-lsp/kotlin-lsp.sh"
+
+    chmod +x "$out"/libexec/kotlin-lsp/kotlin-lsp.sh
+    makeWrapper "$out"/libexec/kotlin-lsp/kotlin-lsp.sh "$out"/bin/kotlin-lsp \
+      --prefix PATH : "${jdk21_headless}/bin"
 
     runHook postInstall
   '';
