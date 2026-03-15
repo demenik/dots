a6808aa75f6eed86acae7c8d7ff4aad27840f5b4
demenik <mail@demenik.dev>
2026-03-15 21:54:28 +0100

    feat(nvim): Add indent fallback

--- home/editors/nvim/plugins/treesitter/default.nix
+++ home/editors/nvim/plugins/treesitter/default.nix
@@ -12,5 +12,32 @@
       ts-autotag.enable = true;
       treesitter-textobjects = import ./textobjects.nix;
     };
+
+    extraConfigLua = ''
+      _G.smart_indent_wrapper = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local lang = vim.treesitter.language.get_lang(vim.bo[bufnr].filetype) or vim.bo[bufnr].filetype
+
+        local has_query = pcall(vim.treesitter.query.get, lang, "indents")
+        local query = has_query and vim.treesitter.query.get(lang, "indents")
+
+        if query then
+          return vim.treesitter.indent()
+        else
+          return vim.fn.cindent(vim.v.lnum)
+        end
+      end
+    '';
+    autoCmd = [
+      {
+        event = "FileType";
+        pattern = ["cs" "kotlin"];
+        command = "setlocal indentexpr=v:lua.smart_indent_wrapper()";
+      }
+    ];
+    opts = {
+      smartindent = true;
+      cindent = true;
+    };
   };
 }
