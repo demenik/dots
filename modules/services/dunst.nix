c82efc9e804e292111c3177d18a73bd2c5e88ea0
demenik <mail@demenik.dev>
2026-03-20 20:16:26 +0100

    feat(mods/dunst): Add dunst module

---
+++ modules/services/dunst.nix
@@ -0,0 +1,53 @@
+{
+  name = "dunst";
+
+  home = {config, ...}: {
+    services.dunst = {
+      enable = true;
+
+      iconTheme = {
+        inherit (config.gtk.iconTheme) name package;
+      };
+
+      settings = let
+        inherit (config) colors;
+      in {
+        global = {
+          width = 400;
+          offset = "5x5";
+          corner_radius = 8;
+          transparency = 10;
+
+          progress_bar_min_width = 380;
+          progress_bar_max_width = 380;
+          progress_bar_corner_radius = 8;
+
+          padding = 10;
+          horizontal_padding = 10;
+          frame_width = 2;
+          gap_size = 3;
+        };
+
+        urgency_low = {
+          format = "<b><span>%s</span></b>\n%b";
+        };
+
+        urgency_normal = {
+          highlight = colors.base0A;
+          default_icon = "dialog-information";
+          format = "<b><span>%s</span></b>\n%b";
+        };
+
+        urgency_critical = {
+          highlight = colors.base08;
+          default_icon = "dialog-error";
+          format = "<b><span>%s</span></b>\n%b";
+        };
+      };
+    };
+
+    wayland.windowManager.hyprland.settings.layerrule = [
+      "animation popin 50%, match:namespace notifications"
+    ];
+  };
+}
