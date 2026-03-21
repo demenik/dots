0eba85574b34d7714064185b7ddcb978ffb187f0
demenik <mail@demenik.dev>
2026-03-21 19:09:23 +0100

    feat(mods/osu): Add opentabletdriver

--- modules/programs/gaming/games/osu.nix
+++ modules/programs/gaming/games/osu.nix
@@ -3,6 +3,16 @@
 
   modules = [../default.nix];
 
+  nixos = {
+    hardware.opentabletdriver = {
+      enable = true;
+      daemon.enable = false;
+    };
+  };
+  hostInstructions = ''
+    Install opentabletdriver
+  '';
+
   home = {pkgs, ...}: {
     home.packages = [pkgs.osu-lazer-bin];
   };
