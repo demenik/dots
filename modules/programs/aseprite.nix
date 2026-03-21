308be6ed15bc7d45005f5746b90341ffb0db2c7e
demenik <mail@demenik.dev>
2026-03-21 20:42:53 +0100

    fix(mods/aseprite): Refactor config and add check for single dir archive

--- modules/programs/aseprite.nix
+++ modules/programs/aseprite.nix
@@ -2,23 +2,34 @@
   name = "asperite";
 
   home = {pkgs, ...}: let
-    catppuccinThemeArchive = pkgs.fetchurl {
-      url = "https://github.com/catppuccin/aseprite/releases/download/v1.2.1/catppuccin-theme-mocha.aseprite-extension";
-      hash = "sha256-embvGNZaGGim9JpWaigbIkt5NxHXpSI+2AaNkgKwMK4=";
-    };
-    catppuccinTheme =
-      pkgs.runCommand "extract-catppuccin-theme" {
+    mkExtension = name: archive:
+      pkgs.runCommand "aseprite-ext-${name}" {
         nativeBuildInputs = [pkgs.unzip];
       } ''
         mkdir -p "$out"
-        unzip "${catppuccinThemeArchive}" -d "$out"
+
+        tmpdir=$(mktemp -d)
+        unzip "${archive}" -d "$tmpdir"
+
+        shopt -s dotglob
+        entries=("$tmpdir"/*)
+        if [ ''${#entries[@]} -eq 1 ] && [ -d "''${entries[0]}" ]; then
+          mv "''${entries[0]}"/* "$out/"
+        else
+          mv "$tmpdir"/* "$out/"
+        fi
       '';
   in {
     home.packages = [pkgs.aseprite];
 
-    xdg.configFile."aseprite/extensions/catppuccin-mocha" = {
-      source = catppuccinTheme;
-      recursive = true;
+    xdg.configFile = {
+      "aseprite/extensions/catppuccin-mocha" = {
+        source = mkExtension "catppuccin-mocha" (pkgs.fetchurl {
+          url = "https://github.com/catppuccin/aseprite/releases/download/v1.2.1/catppuccin-theme-mocha.aseprite-extension";
+          hash = "sha256-embvGNZaGGim9JpWaigbIkt5NxHXpSI+2AaNkgKwMK4=";
+        });
+        recursive = true;
+      };
     };
   };
 }
