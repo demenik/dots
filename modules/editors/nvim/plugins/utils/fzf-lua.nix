bd03734954c5f419345f2b7b62d0b9b5c4b3f359
demenik <mail@demenik.dev>
2026-03-28 18:48:27 +0100

    fix(mods/nvim): Add --hidden to rg_opts in fzf-lua

--- modules/editors/nvim/plugins/utils/fzf-lua.nix
+++ modules/editors/nvim/plugins/utils/fzf-lua.nix
@@ -8,6 +8,7 @@
           git_icons = true;
           file_icons = true;
         };
+        grep.rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e --hidden";
       };
     };
 
