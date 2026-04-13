57bce9645e53fd4f9c3c437487b989e9f5c0a78a
demenik <mail@demenik.dev>
2026-04-13 16:53:26 +0200

    fix(hosts/zeus): Set env vars for nix portable

--- hosts/zeus/default.nix
+++ hosts/zeus/default.nix
@@ -13,5 +13,10 @@
       pubKeyPath = "~/.ssh/id_ed25519.pub";
       pubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPu68pHva1HqWiO4NRjI397+/xSxp5Vv9sFmIvXMKgfP tct47@zeus";
     };
+
+    home.sessionVariables = {
+      NP_RUNTIME = "proot";
+      SSL_CERT_FILE = "/etc/ssl/ca-bundle.pem";
+    };
   };
 }
