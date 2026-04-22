f5d82a23fc5498cb74162f95955a92181bc6dd21
demenik <mail@demenik.dev>
2026-04-22 10:35:42 +0200

    fix(hosts/thinkpad): Set power-saver as default

--- hosts/thinkpad/battery.nix
+++ hosts/thinkpad/battery.nix
@@ -1,7 +1,24 @@
-{lib, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services = {
     upower.enable = true;
     power-profiles-daemon.enable = true;
     tlp.enable = lib.mkForce false;
+  };
+
+  systemd.services.power-saver-default = {
+    description = "Enforce power-saver profile for power-profiles-daemon";
+    wantedBy = ["multi-user.target"];
+    after = ["power-profiles-daemon.service"];
+    requires = ["power-profiles-daemon.service"];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${lib.getExe pkgs.power-profiles-daemon} set power-saver";
+      RemainAfterExit = "yes";
+    };
   };
 }
