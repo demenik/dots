3975c285e9b9d04fbb1d60956faabbe6ca88d913
demenik <mail@demenik.dev>
2026-04-29 13:09:52 +0200

    feat(mods/noctalia): Add plugin settings for vpn

--- modules/wm/.shells/noctalia/plugins/vpn.nix
+++ modules/wm/.shells/noctalia/plugins/vpn.nix
@@ -6,6 +6,13 @@
         sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
       };
     };
+
+    pluginSettings.network-manager-vpn = {
+      displayMode = "alwaysHide";
+      disconnectedColor = "none";
+      connectedColor = "primary";
+      disableToastNotifications = false;
+    };
   };
 
   home.packages = with pkgs; [
