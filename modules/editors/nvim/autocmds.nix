e26c2cc4fda54388fe041afc7da47bbbd1890809
demenik <mail@demenik.dev>
2026-05-01 17:23:05 +0200

    fix(mods/nvim): Make shebang auto cmd more general

--- modules/editors/nvim/autocmds.nix
+++ modules/editors/nvim/autocmds.nix
@@ -10,12 +10,14 @@
           local line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
           if not line then return end
 
-          if line:match("^#!.*bin/env%s+python") or line:match("^#!.*bin/python") then
-            vim.bo.filetype = "python"
-          elseif line:match("^#!.*bin/sh") then
-            vim.bo.filetype = "sh"
-          elseif line:match("^#!.*bin/bash") or line:match("^#!.*bin/env%s+bash") then
-            vim.bo.filetype = "bash"
+          local prog = line:match("^#!.*bin/env%s+([^%s]+)")
+          if not prog then
+            prog = line:match("^#!.*/([^%s]+)")
+          end
+
+          if prog then
+            local ft = prog:gsub("%d+$", "")
+            vim.bo.filetype = ft
           end
         end
       '';
