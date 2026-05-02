ae623dcae7b929930bdc0937c5489c66b2f37465
demenik <mail@demenik.dev>
2026-05-02 13:13:39 +0200

    fix(mods/nvim): Make otter.nvim only run in markdown, org and nix

--- modules/editors/nvim/plugins/lsp/otter.nix
+++ modules/editors/nvim/plugins/lsp/otter.nix
@@ -1,8 +1,24 @@
 {
-  programs.nixvim.plugins.otter = {
-    enable = true;
-    settings = {
-      handle_leading_whitespace = true;
+  programs.nixvim = {
+    plugins.otter = {
+      enable = true;
+      autoActivate = false;
+
+      settings = {
+        handle_leading_whitespace = true;
+      };
     };
+
+    autoCmd = [
+      {
+        event = ["FileType"];
+        pattern = ["markdown" "norg" "nix"];
+        callback.__raw = ''
+          function()
+            require("otter").activate()
+          end
+        '';
+      }
+    ];
   };
 }
