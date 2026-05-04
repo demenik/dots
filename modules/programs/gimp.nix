c82f008c5e8631ba472293de846755b92e8a01f0
demenik <mail@demenik.dev>
2026-05-04 19:09:21 +0200

    feat(mods/gimp): Configure mime apps for images

--- modules/programs/gimp.nix
+++ modules/programs/gimp.nix
@@ -3,5 +3,15 @@
 
   home = {pkgs, ...}: {
     home.packages = [pkgs.gimp];
+
+    xdg.mimeApps.defaultApplications = {
+      "image/jpeg" = ["gimp.desktop"];
+      "image/png" = ["gimp.desktop"];
+      "image/gif" = ["gimp.desktop"];
+      "image/bmp" = ["gimp.desktop"];
+      "image/tiff" = ["gimp.desktop"];
+      "image/webp" = ["gimp.desktop"];
+      "image/x-tga" = ["gimp.desktop"];
+    };
   };
 }
