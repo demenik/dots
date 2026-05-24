3ca1ff4acfa0dd8f1b1c299978d56b47015f8bf0
demenik <mail@demenik.dev>
2026-05-24 13:51:44 +0200

    feat(mods/hyprland): add theme template

--- modules/wm/hyprland/theme.nix
+++ modules/wm/hyprland/theme.nix
@@ -1,4 +1,8 @@
-{config, ...}: let
+{
+  lib,
+  config,
+  ...
+}: let
   c = config.colors;
 in {
   wayland.windowManager.hyprland.settings.config = {
@@ -9,7 +13,7 @@ in {
       gaps_in = 5;
       gaps_out = 5;
 
-      col = {
+      col = lib.mkIf (config.theme.type == "colorScheme") {
         active_border = "rgb(${c.accent})";
         inactive_border = "rgb(${c.base03})";
       };
@@ -24,4 +28,37 @@ in {
       };
     };
   };
+
+  theme.templates.hyprland = {
+    enable = true;
+    target = "~/.config/hypr/theme.lua";
+    text = ''
+      return {
+        active_border = "rgb({{colors.primary.default.hex_stripped}})",
+        inactive_border = "rgb({{colors.surface_container_highest.default.hex_stripped}})",
+      }
+    '';
+    post_hook = "hyprctl reload";
+  };
+
+  wayland.windowManager.hyprland.extraConfig =
+    lib.mkIf (config.theme.type == "template")
+    # lua
+    ''
+      local function load_colors()
+        local f = loadfile(os.getenv("HOME") .. "/.config/hypr/theme.lua")
+        if f then
+          local colors = f()
+          hl.config({
+            general = {
+              col = {
+                active_border = colors.active_border,
+                inactive_border = colors.inactive_border,
+              },
+            },
+          })
+        end
+      end
+      pcall(load_colors)
+    '';
 }
