f409fd9af5d6974614207dc63d9fe322f08a3931
demenik <mail@demenik.dev>
2026-06-08 20:13:44 +0200

    fix(mods/obsidian): dont force workspace 1

--- modules/programs/obsidian.nix
+++ modules/programs/obsidian.nix
@@ -1,15 +1,6 @@
 {
   name = "obsidian";
 
-  moduleConfig = {
-    wm.windowrules = [
-      {
-        matchClass = "obsidian";
-        workspace = "1";
-      }
-    ];
-  };
-
   home = {pkgs, ...}: {
     home.packages = with pkgs; [obsidian];
 
