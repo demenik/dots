a96166f8b0367330def5bc1d059819f0ee09d6a4
demenik <mail@demenik.dev>
2026-06-15 23:04:20 +0200

    fix(mods/nvim): replace pylyzer with pyright

--- modules/editors/nvim/plugins/lang/python.nix
+++ modules/editors/nvim/plugins/lang/python.nix
@@ -6,12 +6,28 @@
       python312Packages.flake8
     ];
 
-    lsp.servers.pylyzer.enable = true;
+    lsp.servers.pyright.enable = true;
 
     plugins = {
       lint.lintersByFt.python = ["ruff"];
 
       conform-nvim.settings.formatters_by_ft.python = ["black"];
+
+      noice.settings.routes = [
+        {
+          filter = {
+            event = "lsp";
+            kind = "progress";
+            cond.__raw = ''
+              function(message)
+                local client = vim.tbl_get(message.opts, "progress", "client")
+                return client == "pyright"
+              end
+            '';
+          };
+          opts.skip = true;
+        }
+      ];
     };
   };
 }
