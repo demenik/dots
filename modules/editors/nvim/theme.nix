01be9b48386d1b45e04003bfa724a977f1dbe190
demenik <mail@demenik.dev>
2026-07-03 19:13:16 +0200

    feat(mods/nvim): add theme template

--- modules/editors/nvim/theme.nix
+++ modules/editors/nvim/theme.nix
@@ -1,4 +1,9 @@
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   programs.nixvim = {
     colorscheme = "catppuccin";
 
@@ -8,6 +13,10 @@
         flavour = "mocha";
         transparent_background = true;
         show_end_of_buffer = true;
+
+        color_overrides = lib.optionalAttrs (config.theme.type == "template") {
+          mocha.__raw = "(pcall(require, 'theme-colors') and require('theme-colors') or {})";
+        };
 
         integrations = {
           cmp = true;
@@ -72,5 +81,47 @@
           '';
       };
     };
+  };
+
+  theme.templates.nvim = {
+    enable = true;
+    target = "~/.config/nvim/lua/theme-colors.lua";
+    post_hook =
+      # bash
+      ''
+        for socket in /run/user/$(id -u)/nvim*.0; do
+          [ -S "$socket" ] && ${lib.getExe pkgs.neovim} --server "$socket" --remote-send "<C-\><C-N>:lua package.loaded['theme-colors'] = nil; vim.cmd('colorscheme catppuccin')<CR>" || true
+        done
+      '';
+    text = ''
+      return {
+        rosewater = "{{colors.primary.default.hex | blend "#F5E0DC", 0.6 | set_saturation 80}}",
+        flamingo = "{{colors.primary.default.hex | blend "#F2CDCD", 0.6 | set_saturation 80}}",
+        pink = "{{colors.primary.default.hex | blend "#FFC0CB", 0.6 | set_saturation 80}}",
+        mauve = "{{colors.tertiary.default.hex}}",
+        red = "{{colors.error.default.hex}}",
+        maroon = "{{colors.primary.default.hex | blend "#EBA0AC", 0.6 | set_saturation 80}}",
+        peach = "{{colors.primary.default.hex | blend "#FFA500", 0.6 | set_saturation 80}}",
+        yellow = "{{colors.primary.default.hex | blend "#FFFF00", 0.6 | set_saturation 80}}",
+        green = "{{colors.primary.default.hex | blend "#00FF00", 0.6 | set_saturation 80}}",
+        teal = "{{colors.primary.default.hex | blend "#008080", 0.6 | set_saturation 80}}",
+        sky = "{{colors.primary.default.hex | blend "#87CEEB", 0.6 | set_saturation 80}}",
+        sapphire = "{{colors.primary.default.hex | blend "#0F52BA", 0.6 | set_saturation 80}}",
+        blue = "{{colors.primary.default.hex}}",
+        lavender = "{{colors.primary.default.hex | blend "#E6E6FA", 0.6 | set_saturation 80}}",
+        text = "{{colors.on_surface.default.hex}}",
+        subtext1 = "{{colors.on_surface_variant.default.hex}}",
+        subtext0 = "{{colors.on_surface_variant.default.hex}}",
+        overlay2 = "{{colors.outline.default.hex}}",
+        overlay1 = "{{colors.outline_variant.default.hex}}",
+        overlay0 = "{{colors.outline_variant.default.hex}}",
+        surface2 = "{{colors.surface_container_highest.default.hex}}",
+        surface1 = "{{colors.surface_container_high.default.hex}}",
+        surface0 = "{{colors.surface_container.default.hex}}",
+        base = "{{colors.surface.default.hex}}",
+        mantle = "{{colors.surface_container_low.default.hex}}",
+        crust = "{{colors.surface_container_lowest.default.hex}}",
+      }
+    '';
   };
 }
