d2dab7dbd74b26ed51874535b15c4a8163eb9204
demenik <mail@demenik.dev>
2026-07-10 13:37:15 +0200

    chore(mods/bat): update bat catppuccin theme

--- modules/cli/bat.nix
+++ modules/cli/bat.nix
@@ -6,8 +6,8 @@
       bat-theme-catppuccin = final.fetchFromGitHub {
         owner = "catppuccin";
         repo = "bat";
-        rev = "6810349";
-        sha256 = "1y5sfi7jfr97z1g6vm2mzbsw59j1jizwlmbadvmx842m0i5ak5ll";
+        rev = "6810349b28055dce54076712fc05fc68da4b8ec0";
+        sha256 = "sha256-lJapSgRVENTrbmpVyn+UQabC9fpV1G1e+CdlJ090uvg=";
       };
     })
   ];
