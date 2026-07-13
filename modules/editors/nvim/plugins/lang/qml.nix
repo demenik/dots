1b36e8e1330862c3ceaf4c8343bbe5eadc4d2d8d
demenik <mail@demenik.dev>
2026-07-13 15:23:56 +0200

    feat(mods/nvim): add import paths to qmlls based on env vars

--- modules/editors/nvim/plugins/lang/qml.nix
+++ modules/editors/nvim/plugins/lang/qml.nix
@@ -5,6 +5,27 @@
     ];
 
     lsp.servers.qmlls.enable = true;
+    extraConfigLuaPre = ''
+      local import_path = os.getenv("QML_IMPORT_PATH")
+      if import_path then
+        local import_paths = {}
+        for path in string.gmatch(import_path, "[^:]+") do
+          table.insert(import_paths, path)
+        end
+
+        local cmd = { "qmlls" }
+        for _, path in ipairs(import_paths) do
+          table.insert(cmd, "-I")
+          table.insert(cmd, path)
+        end
+
+        if vim.lsp and vim.lsp.config then
+          local qmlls_config = vim.lsp.config["qmlls"] or {}
+          qmlls_config.cmd = cmd
+          vim.lsp.config("qmlls", qmlls_config)
+        end
+      end
+    '';
 
     plugins = {
       lint.lintersByFt.qml = ["qmllint"];
