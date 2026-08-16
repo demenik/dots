1887f88ec73e9d1725d0b25d72d94aa59a33b64d
demenik <mail@demenik.dev>
2026-08-16 13:36:45 +0200

    feat(mods/gaming): add env and args for steam games

--- modules/programs/gaming/steam.nix
+++ modules/programs/gaming/steam.nix
@@ -20,22 +20,40 @@
 
     programs.gaming.wrapper.games = {
       # CS2
-      "steam:730".gamescope = {
-        enable = true;
-        args = {
-          w = "1440";
-          h = "1080";
-          S = "stretch";
+      "steam:730" = {
+        gamescope = {
+          enable = true;
+          args = {
+            w = "1440";
+            h = "1080";
+            S = "stretch";
 
-          # https://github.com/ValveSoftware/gamescope/issues/1636#issuecomment-2568139933
-          backend = "sdl";
+            # https://github.com/ValveSoftware/gamescope/issues/1636#issuecomment-2568139933
+            backend = "sdl";
 
-          force-grab-cursor = true;
-          hdr-enabled = false;
+            force-grab-cursor = true;
+            hdr-enabled = false;
+          };
+        };
+        env = {
+          SDL_VIDEO_MINIMIZE_ON_FOCUS_LOSS = "0";
+          SDL_VIDEODRIVER = "x11";
+          SDL_AUDIO_DRIVER = "pipewire";
+        };
+        extraArgs = ["+mat_disable_fancy_blending 1"];
+      };
+
+      # Overwatch
+      "steam:730" = {
+        mangohud.enable = false;
+        env = {
+          DXVK_CONFIG = "\"dxvk.trackPipelineLifetime = True;\"";
+          __GL_SHADER_DISK_CACHE_SKIP_CLEANUP = "1";
+          __GL_SHADER_DISK_CACHE_SIZE = "10737418240";
+          PROTON_LOCAL_SHADER_CACHE = "1";
+          DXVK_ASYNC = "1";
         };
       };
-      # Overwatch
-      "steam:730".mangohud.enable = false;
     };
   };
 
