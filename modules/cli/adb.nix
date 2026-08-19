ac9716856ef6989fd4b762a0934560ed87e217e4
demenik <mail@demenik.dev>
2026-08-19 22:09:02 +0200

    feat(mods/adb): add android emulator window rules

--- modules/cli/adb.nix
+++ modules/cli/adb.nix
@@ -1,5 +1,30 @@
 {
   name = "android";
+  moduleConfig = {
+    wm.windowrules = [
+      # Main emulator screen
+      {
+        matchClass = "Emulator";
+        matchTitle = "^Android Emulator";
+
+        floating = true;
+        keepAspectRatio = true;
+        center = true;
+        size = [340 680];
+        noInitialFocus = false;
+      }
+      # Emulator controls
+      {
+        matchClass = "Emulator";
+        matchTitle = "^Emulator$";
+
+        floating = true;
+        keepAspectRatio = true;
+        size = [43 404];
+        noInitialFocus = true;
+      }
+    ];
+  };
 
   home = {pkgs, ...}: {
     home.packages = with pkgs; [
