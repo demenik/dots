1876ae6547d8e18d4cbc44c4586660dce0a881c7
demenik <mail@demenik.dev>
2026-08-28 21:24:12 +0200

    feat(flake): add caelestia-shell input

--- flake.nix
+++ flake.nix
@@ -46,6 +46,10 @@
       url = "github:noctalia-dev/noctalia-greeter";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     niri = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
