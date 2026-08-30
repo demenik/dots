b037765aab73d7e35789ecaf9ff9b0c8d4842b2d
demenik <mail@demenik.dev>
2026-08-30 23:10:04 +0200

    feat(mods/herdr): port tmux settings

--- modules/shell/.multiplexer/herdr/default.nix
+++ modules/shell/.multiplexer/herdr/default.nix
@@ -14,7 +14,21 @@
       enable = true;
 
       settings = {
-        keys.prefix = "ctrl+space";
+        keys = {
+          prefix = "ctrl+space";
+
+          focus_pane_left = "alt+left";
+          focus_pane_down = "alt+down";
+          focus_pane_up = "alt+up";
+          focus_pane_right = "alt+right";
+
+          previous_tab = "shift+left";
+          next_tab = "shift+right";
+          indexed.tabs = "ctrl";
+
+          copy_mode = "prefix+v";
+        };
+
         ui.mouse_capture = true;
 
         experimental.kitty_graphics = config.programs.kitty.enable;
